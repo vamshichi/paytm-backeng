09bb0fb1043c48dea88ea719bd7d55953b3aa094
vamshichi <chvamshi03@gmail.com>
2024-06-13 21:39:52 +0530

    Initial commit

---
+++ db.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+const dbURI = process.env.MONGODB_URI;
+
+
+mongoose.connect(dbURI);
+
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User,
+  Account,
+};
