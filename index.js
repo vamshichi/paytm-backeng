5be7d2a584942e35f8e8055851f623e53ecfead9
Vamshi ch <115910730+vamshichi@users.noreply.github.com>
2024-06-15 11:27:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
+     origin:["https://pay-tm-vksr.vercel.app/"],
      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
