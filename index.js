904e8d71068e8ce9f198eaad959bbe6535d0e5bf
Vamshi ch <115910730+vamshichi@users.noreply.github.com>
2024-06-15 11:50:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-     origin:["https://pay-tm-vksr-vamshichis-projects.vercel.app"],
+     origin:["https://paytm-fwch.onrender.com"],
      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
