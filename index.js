6177e05bf0ef2ef4d1be8452e81ed2790780f860
Vamshi ch <115910730+vamshichi@users.noreply.github.com>
2024-06-15 12:11:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-     origin:["https://paytm-fwch.onrender.com"],
+     origin:["https://pay-tm.vercel.app"],
      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
